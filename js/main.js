e81a06fe46d648b4fec36015db1565e4fdd0e7fe
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2023-06-08 23:27:10 +0300

    add data object to addTask

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const taskInput = document.querySelector('#taskInput');
 const tasksList = document.querySelector('#tasksList');
 const emptyList = document.querySelector('#emptyList');
 
+let tasks = [];
+
 form.addEventListener('submit', addTask);
 tasksList.addEventListener('click', deleteTask);
 tasksList.addEventListener('click', doneTask);
@@ -11,8 +13,20 @@ function addTask(e) {
     e.preventDefault();
 
     const taskText = taskInput.value;
-    const taskHTML = `<li class="list-group-item d-flex justify-content-between task-item">
-    <span class="task-title">${taskText}</span>
+
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        done: false,
+    };
+
+    tasks.push(newTask);
+
+    console.log('tasks', tasks);
+    const cssClass = newTask.done ? 'task-title task-title--done' : 'task-title';
+
+    const taskHTML = `<li id="${newTask.id}" class="list-group-item d-flex justify-content-between task-item">
+    <span class="${cssClass}">${newTask.text}</span>
     <div class="task-item__buttons">
         <button type="button" data-action="done" class="btn-action">
             <img src="./img/tick.svg" alt="Done" width="18" height="18">
